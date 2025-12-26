4c205763c5b3adcb4dd7e2e57aca97cf74818aec
= <=>
2025-12-26 09:13:42 +0500

    Fix Django settings for Vercel

--- myDictionary/settings.py
+++ myDictionary/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = False
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wm*nwggw^ejw%0&a&jyf+1ig-1($z-vf+3s@umr0hl&nzg70c4'
+#SECRET_KEY = 'django-insecure-wm*nwggw^ejw%0&a&jyf+1ig-1($z-vf+3s@umr0hl&nzg70c4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.vercel.app'
+]
 
 
 # Application definition
